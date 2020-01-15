dfdd8b5163ea073c53737a5f874e010b584c8426
Chloe Gell <gellert.chloe@gmail.com>
2020-01-15 21:27:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,4 +7,17 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if(board[index])
+  if(board[index] == "" || board[index] == " " || board[index] == null)
+    return true 
+  else
+    return false 
+  end
+end 
+
+def move(board, index, token="X")
+  if((valid_move?(board, index)) == true)
+    board[index] == token
+  else
+    return false 
+  end
+end
