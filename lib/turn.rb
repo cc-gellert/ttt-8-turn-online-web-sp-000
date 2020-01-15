4211bb781985abdbfa895515cecbbec284e0a162
Chloe Gell <gellert.chloe@gmail.com>
2020-01-15 22:00:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def position_taken?(board, index)
 end
 
 def move(board, index, token="X")
-  if((valid_move?(board, index)) == true)
+  if(valid_move?(board, index))
     board[index] == token
   else
     return false 
