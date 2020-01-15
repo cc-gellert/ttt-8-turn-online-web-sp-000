edcbab988dbf6b0021eabb54e4d6412e24aecd45
Chloe Gell <gellert.chloe@gmail.com>
2020-01-15 21:28:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(str)
+  str.toInt
+
 def valid_move?(board, index)
   if(board[index] == "" || board[index] == " " || board[index] == null)
     return true 
